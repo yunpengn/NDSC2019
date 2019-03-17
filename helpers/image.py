ce6308814af8cfd7898ed7e5ee9ce9055e4809cf
CoderStellaJ <40347063+CoderStellaJ@users.noreply.github.com>
2019-03-17 23:02:46 +0800

    preprocess images

---
+++ helpers/image.py
@@ -0,0 +1,12 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+
+transform = transforms.Compose(
+    [transforms.ToTensor(),
+     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+
+trainset = torchvision.datasets.ImageFolder(root='../data/train_set/images', train=True,
+                                        download=True, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
+                                          shuffle=True, num_workers=2)
